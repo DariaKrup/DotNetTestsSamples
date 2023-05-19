023c84b5ee2a7bdcdd7ec544a45742bb5a09b859
admin <admin@TeamCity>
2023-05-19 16:26:39 +0200

    TeamCity change in 'dotnetPar / comp' project: runners of 'dep2' build configuration were updated

--- .teamcity/patches/buildTypes/Comp_Dep2.kts
+++ .teamcity/patches/buildTypes/Comp_Dep2.kts
@@ -2,6 +2,7 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.ui.*
 
 /*
@@ -15,6 +16,12 @@ create(RelativeId("Comp"), BuildType({
 
     vcs {
         root(RelativeId("HttpsGithubComChubatovaTigerChubatovaGradleTestsBackup"))
+    }
+
+    steps {
+        script {
+            scriptContent = "sleep 30"
+        }
     }
 }))
 
