059200971316ecb5eddc7e9581253b2f1aa229ea
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2024-03-06 22:51:19 +0100

    Delete TestProject1 directory

--- TestProject1/generateTests.ps1
+++
@@ -1,5 +0,0 @@
-﻿for (($i = 0); $i -lt 25000; $i++)
-{
- $m='UnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTestUnitTest' + $i;
- (Get-Content .\xUnit.cs) -Replace 'UnitTest1', $m | Set-Content .\xUnit$i.cs
-}
